5d51521781dcbc930c96ee73757284e63daf6212
Robin Camille Davis <robincamilledavis@gmail.com>
2018-06-19 14:21:37 -0400

    Change credentials module name

--- advanced_ala-talk-generator.py
+++ advanced_ala-talk-generator.py
@@ -11,7 +11,7 @@
 
 import random, string
 import tweepy
-from alacredentials import *
+from credentials import *
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
